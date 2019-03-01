2b69c152203f5ffc7ef4d76a43cc48d372a528fd
Vidall <DjangoRaijin22@gmail.com>
2019-02-28 19:40:53 -0500

    created game layout and grabbed neccessary elements

---
+++ main.js
@@ -0,0 +1,5 @@
+let time = document.querySelector("time_num");
+let moves = document.querySelector("moves_num");
+let reset = document.querySelector("reset_btn");
+
+let cardBack = document.querySelectorAll("card_back");
