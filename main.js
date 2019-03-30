33315df5fe719dfed3a490ff4b75909e9754f635
Vidall <DjangoRaijin22@gmail.com>
2019-03-29 21:17:53 -0400

    refactoring entire project

--- main.js
+++ main.js
@@ -1,133 +1,140 @@
-const time = document.querySelector(".time_num");
-const moves = document.querySelector(".moves_num");
-const reset = document.querySelector(".reset_btn");
-const card = document.querySelectorAll(".card");
-//const dificultySelect = document.querySelector(".difficulty");
-const cards = [...card];
+window.onload = function() {
+  const time = document.querySelector(".time_num");
+  const moves = document.querySelector(".moves_num");
+  const reset = document.querySelector(".reset_btn");
+  const card = document.querySelectorAll(".card");
+  //need to grab card front or cant compare images
+  //const dificultySelect = document.querySelector(".difficulty");
+  const cards = [...card];
 
-let isFlipped = false;
-let firstPick, secondPick;
-//game board
-class gameMat {
-  constructor(board, win, turn, time, score) {
-    this.board = Array(12).fill(null);
-    this.win = false;
-    this.turn = 0;
-    this.time = 0;
-    this.score = 0;
+  let isFlipped = false;
+  let firstPick, secondPick;
+  //game board
+  class gameMat {
+    constructor(board, win, turn, time, score) {
+      this.board = Array(12).fill(null);
+      this.win = false;
+      this.turn = 0;
+      this.time = 0;
+      this.score = 0;
+    }
+
+    //get turn
+    getTurn = () => {
+      return this.turn;
+    };
+
+    turnUp = () => {
+      this.turn = this.turn + 1;
+    };
   }
 
-  //get turn
-  getTurn = () => {
-    return this.turn;
+  let DBZcardFont = [
+    {
+      img: "gokuProfile.jpg",
+      name: "goku"
+    },
+    {
+      img: "vegetaProfile.jpg",
+      name: "vegeta"
+    },
+    {
+      img: "krillinProfile.jpg",
+      name: "krillin"
+    },
+    {
+      img: "piccolo.jpg",
+      name: "piccolo"
+    },
+    {
+      img: "bulmaProfile.jpg",
+      name: "bulma"
+    },
+    {
+      img: "gohanProfile.jpg",
+      name: "gohan"
+    }
+  ];
+  //populate game board
+  populate = gameMat => {
+    gameMat.board.forEach(element => {
+      if (element === null) {
+      }
+    });
   };
 
-  turnUp = () => {
-    this.turn = this.turn + 1;
+  //increase turn
+  turnUp = gameMat => {
+    gameMat.turn = gameMat.turn + 1;
   };
-}
+  //timer
+  trackTime = () => {};
+  //increase score
+  increaseScore = () => {};
 
-let DBZcardFont = [
-  {
-    img: "gokuProfile.jpg",
-    num: 1
-  },
-  {
-    img: "vegetaProfile.jpg",
-    num: 2
-  },
-  {
-    img: "krillinProfile.jpg",
-    num: 3
-  },
-  {
-    img: "piccolo.jpg",
-    num: 4
-  },
-  {
-    img: "bulmaProfile.jpg",
-    num: 5
-  },
-  {
-    img: "gohanProfile.jpg",
-    num: 6
-  }
-];
-//populate game board
-populate = gameMat => {
-  gameMat.board.forEach(element => {
-    if (element === null) {
+  //add chosen class to cards
+
+  let flipCard = function() {
+    this.classList.add("chosen");
+    console.log(this.dataset.character);
+    console.log(getComputedStyle(this));
+    console.log(this.childNodes[1].childNodes[1].style.color);
+    this.children[0].children[0].style.backgroundImage = "";
+    if (!isFlipped) {
+      isFlipped = true;
+      firstPick = this;
+      console.log(firstPick);
+      console.log(firstPick.src);
+      console.log(firstPick.style.backgroundColor);
+    } else {
+      secondPick = this;
+      isFlipped = false;
+      console.log(secondPick);
+      console.log(secondPick.style.backgroundImage);
     }
-  });
-};
 
-//increase turn
-turnUp = gameMat => {
-  gameMat.turn = gameMat.turn + 1;
-};
-//timer
-trackTime = () => {};
-//increase score
-increaseScore = () => {};
-
-//add chosen class to cards
-let checkMatch = (firstPick, secondPick) => {
-  if (firstPick.src === secondPick.src) {
-    return true;
-  } else {
-    return false;
-  }
-};
-let flipCard = function() {
-  this.classList.toggle("chosen");
-
-  if (!isFlipped) {
-    isFlipped = true;
-    firstPick = this;
-    console.log(firstPick);
-    console.log(firstPick.src);
-  } else {
-    secondPick = this;
-    isFlipped = false;
-    console.log(secondPick);
-    console.log(secondPick.src);
-  }
-
-  if (checkMatch(firstPick, secondPick) === true) {
-    //lock  the cards by removing event listner and add point to the score
-    firstPick.removeEventListner("click", flipCard);
-    secondPick.removeEventListner("click", flipCard);
-  }
-};
-
-for (let i = 0; i < cards.length; i++) {
-  cards[i].addEventListener("click", flipCard);
-}
-
-//difficulties
-let selectDifficulty = () => {
-  if (dificultySelect === 1) {
-    let gameMat1 = new gameMat();
-  } else if (difficultySelect === 2) {
-    let gameMat1 = new gameMat();
-    let gameMat2 = new gameMat();
-  } else {
-    let gameMat1 = new gameMat();
-    let gameMat2 = new gameMat();
-    let gameMat3 = new gameMat();
-  }
-};
-//test board
-let testBoard = new gameMat();
-time.textContent = testBoard.time;
-testBoard.turnUp();
-console.log(testBoard.turn);
-//start game if board win state is false
-let startGame = gameMat => {
-  if (win != true) {
-    populate(gameMat);
-    while (gameMat.win != true) {
-      time.textContent = gameMat.time;
+    checkMatch(firstPick, secondPick);
+  };
+  let checkMatch = (firstPick, secondPick) => {
+    if (firstPick.style.backgroundImage === secondPick.style.backgroundImage) {
+      firstPick.removeEventListner("click", flipCard);
+      secondPick.removeEventListner("click", flipCard);
+      return true;
+    } else {
+      return false;
     }
+  };
+
+  //add Event Listener to cards
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", flipCard);
   }
+
+  //difficulties
+  let selectDifficulty = () => {
+    if (dificultySelect === 1) {
+      let gameMat1 = new gameMat();
+    } else if (difficultySelect === 2) {
+      let gameMat1 = new gameMat();
+      let gameMat2 = new gameMat();
+    } else {
+      let gameMat1 = new gameMat();
+      let gameMat2 = new gameMat();
+      let gameMat3 = new gameMat();
+    }
+  };
+  //test board
+  let testBoard = new gameMat();
+  time.textContent = testBoard.time;
+  testBoard.turnUp();
+  console.log(testBoard.turn);
+  //start game if board win state is false
+  let startGame = gameMat => {
+    if (win != true) {
+      populate(gameMat);
+      while (gameMat.win != true) {
+        time.textContent = gameMat.time;
+      }
+    }
+  };
 };
